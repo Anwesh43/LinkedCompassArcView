3b3fb1a9793732bd8f7ccb79459c871174d8819c
Anwesh43 <anweshthecool0@gmail.com>
2018-09-29 11:09:37 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedcompassarcview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcompassarcview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedcompassarcview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.compassarcview.CompassArcView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CompassArcView.create(this)
     }
 }
