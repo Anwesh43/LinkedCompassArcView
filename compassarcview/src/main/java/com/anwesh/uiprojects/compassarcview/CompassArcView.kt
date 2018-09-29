083fd1c0985c2f6e1d84fa360af362db351ddff2
Anwesh43 <anweshthecool0@gmail.com>
2018-09-29 10:56:27 +0530

    adding CANode class

--- compassarcview/src/main/java/com/anwesh/uiprojects/compassarcview/CompassArcView.kt
+++ compassarcview/src/main/java/com/anwesh/uiprojects/compassarcview/CompassArcView.kt
@@ -111,4 +111,47 @@ class CompassArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CANode(var i : Int, var state : State = State()) {
+        private var next : CANode? = null
+        private var prev : CANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = CANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CANode {
+            var curr : CANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCANode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+    }
 }
